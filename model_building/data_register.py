5a218981f6a6c61da410dece8c2f4544641d8994
Sivavvpgit <etttestcontrol@gmail.com>
2026-04-06 12:13:42 +0530

    Update data_register.py

--- model_building/data_register.py
+++ model_building/data_register.py
@@ -19,7 +19,7 @@ except RepositoryNotFoundError:
     print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
-    folder_path="/content/drive/MyDrive/engine_failure_prediction/data",
+    folder_path="data",
     repo_id=repo_id,
     repo_type=repo_type,
 )
